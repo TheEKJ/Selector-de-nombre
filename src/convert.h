0cd41cd386d3b85688eb143417206769a80990ad
TheEKJ <the_ekj.2121@outlook.com>
2021-03-30 11:25:17 -0500

    Update convert.h

--- src/convert.h
+++ src/convert.h
@@ -10,24 +10,46 @@ namespace sn::details
    constexpr inline int to_win32(Color color)
    {
       static_assert(
-      (int)DkBlue    == 1  &&
-      (int)DkGreen   == 2  &&
-      (int)DkCyan    == 3  &&
-      (int)DkRed     == 4  &&
-      (int)DkViolet  == 5  &&
-      (int)DkYellow  == 6  &&
-      (int)DkWhite   == 7  &&
-      (int)DkGray    == 8  &&
-      (int)Blue      == 9  &&
-      (int)Green     == 10 &&
-      (int)Cyan      == 11 &&
-      (int)Red       == 12 &&
-      (int)Violet    == 13 &&
-      (int)Yellow    == 14 &&
-      (int)White     == 15,
-      "Color not match with Win32!");
+         (int)DkBlue    == 1  &&
+         (int)DkGreen   == 2  &&
+         (int)DkCyan    == 3  &&
+         (int)DkRed     == 4  &&
+         (int)DkViolet  == 5  &&
+         (int)DkYellow  == 6  &&
+         (int)DkWhite   == 7  &&
+         (int)DkGray    == 8  &&
+         (int)Blue      == 9  &&
+         (int)Green     == 10 &&
+         (int)Cyan      == 11 &&
+         (int)Red       == 12 &&
+         (int)Violet    == 13 &&
+         (int)Yellow    == 14 &&
+         (int)White     == 15,
+         "Color not match with Win32!");
 
       return (int)color;
+   }
+
+   constexpr inline Color to_win32(DWORD attributes)
+   {
+      static_assert(
+         (int)DkBlue    == 1  &&
+         (int)DkGreen   == 2  &&
+         (int)DkCyan    == 3  &&
+         (int)DkRed     == 4  &&
+         (int)DkViolet  == 5  &&
+         (int)DkYellow  == 6  &&
+         (int)DkWhite   == 7  &&
+         (int)DkGray    == 8  &&
+         (int)Blue      == 9  &&
+         (int)Green     == 10 &&
+         (int)Cyan      == 11 &&
+         (int)Red       == 12 &&
+         (int)Violet    == 13 &&
+         (int)Yellow    == 14 &&
+         (int)White     == 15,
+         "Color not match with Win32!");
+      return (Color)attributes;
    }
 
    constexpr inline COORD to_win32(const Point& point)
