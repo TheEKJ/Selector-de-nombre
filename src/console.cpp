0858fb77dd12d172f33a71b6309f8195f982e5be
TheEKJ <the_ekj.2121@outlook.com>
2021-04-01 20:22:00 -0500

    Added charbuffer functions

--- src/console.cpp
+++ src/console.cpp
@@ -2,7 +2,10 @@
 
 #ifdef _WIN32
    #include "win/win_console.h"
+   #include "win/char_buffer.h"
+
    typedef sn::WinConsole ConsoleImplType;
+   typedef sn::WinCharBuffer CharBufferType;
 #endif
 
 namespace sn
@@ -12,5 +15,10 @@ namespace sn
    Console* Console::GetInstance()
    {
       return &g_console;
+   }
+
+   std::unique_ptr<CharBuffer> Console::createChrBuffer(std::initializer_list<char> buffer, const Rect& rc)
+   {
+      return std::unique_ptr<CharBuffer>(new CharBufferType(buffer, rc));
    }
 } // namespace sn
